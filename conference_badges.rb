ad51c3771d8569f16906a1e23b593fa6bbb2a932
Lauren Pardee <lap5151@gmail.com>
2018-02-16 20:01:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def name_array
+  ["Edsger", "Ada", "Charles", "Alan, Grace", "Linus", "Matz"]
+end
+
+def badge_batch_array(array)
+    new_array = []
+    array.each do |person|
+    new_array.push("Hello, my name is #{person}.")
+  end
+  return new_array
+end
